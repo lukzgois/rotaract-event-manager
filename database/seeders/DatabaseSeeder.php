6a88bf80e60c05248749dbe444bc3ef4cfd31957
Lucas Padilha Gois <lucaspgois@gmail.com>
2023-03-28 22:06:46 -0300

    Adding seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $clubs = \App\Models\Club::factory(3)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(15)
+            ->sequence(
+                ['club_id' => $clubs[0]->id],
+                ['club_id' => $clubs[1]->id],
+                ['club_id' => $clubs[2]->id],
+            )
+            ->create();
+
+        \App\Models\User::factory([
+            'club_id' => $clubs[0]->id,
+            'email' => 'admin@test.com',
+            'user_type' => 'admin'
+        ])
+        ->create();
     }
 }
