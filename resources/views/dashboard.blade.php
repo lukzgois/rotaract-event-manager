95ad829ea2d01614eb19d2e41a11cfd31d3b40a0
Lucas Padilha Gois <lucaspgois@gmail.com>
2023-04-17 23:13:28 -0300

    Adding PIX information to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,10 +37,24 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h2 class="text-xl font-bold mb-4">Pagamento via PIX</h2>
 
-                    <p>
-                        Chave PIX: <strong>fematakas@gmail.com</strong><br>
-                        Nome: Fernanda Gurgel Matakas
-                    </p>
+                    <div class="flex items-center">
+                        <div class="mr-4 basis-1/2">
+                            <img src="{{ Vite::asset('resources/images/qrcode-1-lote.jpeg') }}" />
+                        </div>
+
+                        <div>
+                            <p class="my-2">Chave PIX: <strong>fematakas@gmail.com</strong></p>
+                            <p class="my-2">Valor: <strong>R$ {{ ENV('TICKET_BATCH_PRICE')}}</strong></p>
+                            <p class="my-2">Nome: <strong>Fernanda Gurgel Matakas</strong><p>
+                            <p class="my-2">
+                                Pix copia e cola: <br>
+                                <strong>
+                                    00020126700014br.gov.bcb.pix0114+55429997980020230Inscricao CODIRC primeiro lote5204000053039865406180.005802BR5923FERNANDA GURGEL MATAKAS6010GUARAPUAVA62290525Pe0WEiNx4AqMBQWqqeiNTD1Sk63040761
+                                </strong>
+                            <p>
+                            <a href="#" class="text-blue-500 text-sm underline" onclick="copyPixCode(); return false">Copiar</a>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -64,3 +78,9 @@ data-preference-id="168858925-a33f91ac-3a1b-4a1c-b37a-4ff81e543c69" data-source=
         </div>
     </div>
 </x-app-layout>
+
+<script>
+function copyPixCode() {
+    navigator.clipboard.writeText('00020126700014br.gov.bcb.pix0114+55429997980020230Inscricao CODIRC primeiro lote5204000053039865406180.005802BR5923FERNANDA GURGEL MATAKAS6010GUARAPUAVA62290525Pe0WEiNx4AqMBQWqqeiNTD1Sk63040761')
+}
+</script>
