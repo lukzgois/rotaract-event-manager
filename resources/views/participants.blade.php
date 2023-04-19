691475f2a69a4fc48554220cc0fb97cf7cebbe74
Lucas Padilha Gois <lucaspgois@gmail.com>
2023-04-19 17:12:37 -0300

    Fixing style on mobile

--- resources/views/participants.blade.php
+++ resources/views/participants.blade.php
@@ -1,11 +1,11 @@
 <x-pages-layout>
-    <div class="p-4 text-white relative flex flex-col justify-center items-center h-screen">
+    <div class="text-white relative flex flex-col justify-center items-center">
         <a href="/" class="rounded-full bg-white/30 w-32 h-32 flex items-center justify-center">
             <img src="{{ Vite::asset('resources/images/logo-120x175.png') }}" width="75" />
         </a>
 
-        <div class="text-white mt-6 bg-sky-800/50 p-4 rounded-lg">
-            <table class="table-auto text-lg">
+        <div class="text-white mt-6 bg-sky-800/50 rounded-lg md:p-4">
+            <table class="text-lg">
                 <thead>
                     <tr class="h-12 text-xl font-bold border-b">
                         <th class="text-left">Nome</th>
@@ -15,10 +15,10 @@
                 </thead>
                 <tbody>
                     @foreach($participants as $participant)
-                        <tr>
-                            <td class="pr-16">{{ $participant->name }}</td>
+                        <tr class="border-b border-dashed border-white/50">
+                            <td class="py-8 pr-16">{{ $participant->name }}</td>
                             <td class="pr-16">{{ $participant->nickname }}</td>
-                            <td class="pr-4">{{ $participant->club_name }}</td>
+                            <td class="">{{ $participant->club_name }}</td>
                         </tr>
                     @endforeach
                 </tbody>
