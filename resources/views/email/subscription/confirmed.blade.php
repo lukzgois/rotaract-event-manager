1f5c917e36e4f9e40526084f449ad1cd54d6d08d
Lucas Padilha Gois <lucaspgois@gmail.com>
2023-05-11 15:12:41 -0300

    Enhancing mail template

--- resources/views/email/subscription/confirmed.blade.php
+++ resources/views/email/subscription/confirmed.blade.php
@@ -1,12 +1,14 @@
-<h1>Bem vindo a bordo, {{ $subscription->user->name }}!</h1>
+<x-mail-layout>
+    <h1>Bem vindo a bordo, {{ $subscription->user->name }}!</h1>
 
-<p>
-    Sua inscrição foi confirmada e estamos ansios para receber você e seus companheiros de bordo em nosso cais.<br>
-    Confira seus mantimentos, ice as velas e venha desvendar os mistérios dos sete mares.
-</p>
+    <p>
+        Sua inscrição foi confirmada e estamos ansios para receber você e seus companheiros de bordo em nosso cais.<br>
+        Confira seus mantimentos, ice as velas e venha desvendar os mistérios dos sete mares.
+    </p>
 
-<p>
-    Não se esqueça de seguir o instagram oficial do evento para se manter atualizado, estamos postando várias coisas por lá (e ouvi dizer que tem até uma série sobre um certo pirata e sua tripulação).<br><br>
-    <a href="https://www.instagram.com/codircdossetemares/">CODIRC dos sete mares</a> <br>
-    <a href="https://www.instagram.com/eventos_4640">Eventos 4640</a>
-</p>
+    <p>
+        Não se esqueça de seguir o instagram oficial do evento para se manter atualizado, estamos postando várias coisas por lá (e ouvi dizer que tem até uma série sobre um certo pirata e sua tripulação).<br><br>
+        <a href="https://www.instagram.com/codircdossetemares/">CODIRC dos sete mares</a> <br>
+        <a href="https://www.instagram.com/eventos_4640">Eventos 4640</a>
+    </p>
+</x-mail-layout>
