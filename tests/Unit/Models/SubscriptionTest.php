fa35169188f83306b4c553b2c6c0974487705dd7
Lukz <lucaspgois@gmail.com>
2023-05-15 16:44:30 -0300

    Adding tests to the subscription model

--- tests/Unit/Models/SubscriptionTest.php
+++ tests/Unit/Models/SubscriptionTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Club;
 use App\Models\Subscription;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 
 it('belongs to a user', function () {
     $user = User::factory()->forClub()->create();
@@ -36,4 +38,37 @@ test('isPending(): returns true if paid_at is not set', function() {
     $subscription = Subscription::factory()->for($user)->create();
 
     expect($subscription->isPending())->toBeTrue();
+});
+
+it('returns the totals of confirmed/pending subscriptions grouped by club', function () {
+    $clubs = Club::factory()->count(2)->create();
+    User::factory(4)
+        ->hasSubscription()
+        ->sequence(
+            ['club_id' => $clubs[0]->id],
+            ['club_id' => $clubs[1]->id],
+        )
+        ->create();
+    User::factory([
+        'club_id' => $clubs[0]->id,
+        'email' => 'paid@test.com',
+    ])
+    ->hasSubscription(['paid_at' => Carbon::now()])
+    ->create();
+
+    $totals = Subscription::totalPerClub();
+
+    expect($totals)->toHaveLength(2);
+    expect($totals->toArray())->toContain([
+        'name' => $clubs[0]->name,
+        'total' => 3,
+        'pending' => 2,
+        'paid' => 1
+    ]);
+    expect($totals->toArray())->toContain([
+        'name' => $clubs[1]->name,
+        'total' => 2,
+        'pending' => 2,
+        'paid' => 0
+    ]);
 });
